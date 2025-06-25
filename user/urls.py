17359e114ca2730d03c5c1f25ccd30f660f74202
naym00 <naymhsain00@gmail.com>
2025-06-26 02:22:45 +0600

    unique username

--- user/urls.py
+++ user/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('forgot-password/', views.forgot_password, name='forgot-password'),
     path('set-password/', views.set_password, name='set-password'),
     path('logout/', views.user_logout, name='logout'),
+    
+    path('unique-username/<str:username>/', views.unique_username, name='unique-username'),
 ]
--- user/views.py
+++ user/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, redirect
 from user import models as MODELS_USER
 from settings import models as MODELS_SETT
 from help.common.generic import ghelp
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 def user_register(request):
     html_path = 'dictionary/user/register.html'
@@ -203,3 +206,10 @@ def set_password(request):
 def user_logout(request):
     logout(request)
     return redirect('home')
+
+@api_view(['GET'])
+def unique_username(request, username=None):
+    existance_of_username = False
+    if username != None:
+        existance_of_username = MODELS_USER.User.objects.filter(username=username).exists()
+    return Response({'is_exist': existance_of_username}, status=status.HTTP_200_OK)
